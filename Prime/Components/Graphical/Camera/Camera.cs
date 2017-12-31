9cc321b6408ab1c71384ff66fff984c0f956b7c3
Pietro Carrara <piticarrara@gmail.com>
2017-12-31 14:09:10 -0200

    Add mouse input

--- Prime/Components/Graphical/Camera/Camera.cs
+++ Prime/Components/Graphical/Camera/Camera.cs
@@ -1,3 +1,4 @@
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended;
 using MonoGame.Extended.ViewportAdapters;
@@ -6,10 +7,29 @@ namespace Prime
 {
 	public class Camera : Camera2D
 	{
+		public ViewportAdapter ViewportAdapter;
+
 		public Camera(GraphicsDevice graphicsDevice) : base(graphicsDevice)
         {  }
 
         public Camera(ViewportAdapter viewportAdapter) : base(viewportAdapter)
-        {  }
+		{
+			this.ViewportAdapter = viewportAdapter;
+		}
+
+		
+
+		public new Vector2 Position
+		{
+			set
+			{
+				base.Position = value - new Vector2(1280 / 2f, 720 / 2f);
+			}
+			
+			get
+			{
+				return base.Position + new Vector2(1280 / 2f, 720 / 2f);
+			}
+		}
 	}
 }
--- Prime/Systems/Input.cs
+++ Prime/Systems/Input.cs
@@ -1,3 +1,4 @@
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
 namespace Prime
@@ -7,10 +8,16 @@ namespace Prime
 		private static KeyboardState 	kbdState = Keyboard.GetState(),
 								prevKbdState;
 
+		private static MouseState 	mouse = Mouse.GetState(),
+									prevMouse;
+
 		internal static void Update()
 		{
 			prevKbdState = kbdState;
 			kbdState = Keyboard.GetState();
+
+			prevMouse = mouse;
+			mouse = Mouse.GetState();
 		}
 
 		public static bool IsKeyDown(Keys k)
@@ -36,5 +43,117 @@ namespace Prime
 		{
 			return prevKbdState.IsKeyDown(k) && kbdState.IsKeyUp(k);
 		}
+
+		public static bool IsButtonDown(MouseButtons b)
+		{
+			ButtonState btState;
+
+			switch(b)
+			{
+				case MouseButtons.Middle:
+					btState = mouse.MiddleButton;
+					break;
+				case MouseButtons.Left:
+					btState = mouse.LeftButton;
+					break;
+				case MouseButtons.Right:
+					btState = mouse.RightButton;
+					break;
+				default:
+					return false;
+			}
+
+			return btState == ButtonState.Pressed;
+		}
+
+		public static bool IsButtonUp(MouseButtons b)
+		{
+			ButtonState btState;
+
+			switch(b)
+			{
+				case MouseButtons.Middle:
+					btState = mouse.MiddleButton;
+					break;
+				case MouseButtons.Left:
+					btState = mouse.LeftButton;
+					break;
+				case MouseButtons.Right:
+					btState = mouse.RightButton;
+					break;
+				default:
+					return false;
+			}
+
+			return btState == ButtonState.Released;
+		}
+
+		public static bool IsButtonPressed(MouseButtons b)
+		{
+			ButtonState btState, prevBtState;
+
+			switch(b)
+			{
+				case MouseButtons.Middle:
+					btState = mouse.MiddleButton;
+					prevBtState = prevMouse.MiddleButton;
+					break;
+				case MouseButtons.Left:
+					btState = mouse.LeftButton;
+					prevBtState = prevMouse.LeftButton;
+					break;
+				case MouseButtons.Right:
+					btState = mouse.RightButton;
+					prevBtState = prevMouse.RightButton;
+					break;
+				default:
+					return false;
+			}
+
+			return btState == ButtonState.Pressed && prevBtState == ButtonState.Released;
+		}
+
+		public static bool IsButtonReleased(MouseButtons b)
+		{
+			ButtonState btState, prevBtState;
+
+			switch(b)
+			{
+				case MouseButtons.Middle:
+					btState = mouse.MiddleButton;
+					prevBtState = prevMouse.MiddleButton;
+					break;
+				case MouseButtons.Left:
+					btState = mouse.LeftButton;
+					prevBtState = prevMouse.LeftButton;
+					break;
+				case MouseButtons.Right:
+					btState = mouse.RightButton;
+					prevBtState = prevMouse.RightButton;
+					break;
+				default:
+					return false;
+			}
+
+			return btState == ButtonState.Released && prevBtState == ButtonState.Pressed;
+		}
+
+		public static Vector2 MousePosition()
+		{
+			return mouse.Position.ToVector2();
+		}
+
+		public static Vector2 MousePosition(Camera c)
+		{
+			
+			return c.ViewportAdapter.PointToScreen(mouse.Position).ToVector2() - c.Position - new Vector2(1280 / 2f, 720 / 2f);
+		}
+	}
+
+	public enum MouseButtons
+	{
+		Middle,
+		Left,
+		Right
 	}
 }
