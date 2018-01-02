64c9c07201cc778512ca7949a62aa7a0c41e9088
Pietro Carrara <piticarrara@gmail.com>
2018-01-02 10:14:45 -0200

    Add prototyping sprites

---
+++ Prime/Components/Graphical/Sprites/Prototyping/RectangleSprite.cs
@@ -0,0 +1,46 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Prime.Graphics
+{
+	public class RectangleSprite : Sprite
+	{
+		public Color Color = Color.Red;
+
+		private int width, height;
+
+		public RectangleSprite(int w, int h)
+		{
+			this.width = w;
+			this.height = h;
+
+			base.Origin = new Vector2(w / 2f, h / 2f);
+		}
+
+		public RectangleSprite(int w, int h, Color c) : this(w, h)
+		{
+			this.Color = c;
+		}
+
+		public override void Initialize()
+		{
+			base.Initialize();
+
+			if (this.Owner == null)
+				throw new System.Exception();
+
+			var tex = new Texture2D(this.Owner.Scene.Game.GraphicsDevice, width, height);
+
+			var color = new Color[width * height];
+
+			for (int i = 0; i < width * height; i++)
+			{
+				color[i] = this.Color;
+			}
+
+			tex.SetData(color);
+
+			base.Tex = tex;
+		}
+	}
+}
