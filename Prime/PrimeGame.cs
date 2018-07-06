f971e6647b8db3100ff2fd007f9075abcf820eca
Pietro Carrara <piticarrara@gmail.com>
2018-07-06 10:51:55 -0300

    Fix black screen when using GeonBit's UI

--- Prime/PrimeGame.cs
+++ Prime/PrimeGame.cs
@@ -4,11 +4,12 @@ using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended;
 using MonoGame.Extended.ViewportAdapters;
 using GeonBit.UI;
+using GeonBit.UI.Entities;
 
 namespace Prime
 {
-    public class PrimeGame : Game
-    {
+	public class PrimeGame : Game
+	{
 		public static PrimeGame Game;
 
 		// The spritebatch that draws tem all
@@ -54,14 +55,14 @@ namespace Prime
 			}
 		}
 
-        public PrimeGame(Scene s)
-        {
+		public PrimeGame(Scene s)
+		{
 			Game = this;
 
-            graphics = new GraphicsDeviceManager(this);
+			graphics = new GraphicsDeviceManager(this);
 			this.Window.AllowUserResizing = true;
 
-            Content.RootDirectory = "Content";
+			Content.RootDirectory = "Content";
 
 			activeScene = s;
 			activeScene.Game = this;
@@ -69,48 +70,45 @@ namespace Prime
 			activeScene.Content.RootDirectory = "Content";
 
 			base.IsMouseVisible = true;
-        }
+		}
 
-        protected override void Initialize()
-        {
-            base.Initialize();
+		protected override void Initialize()
+		{
+			base.Initialize();
 
 			this.viewPortAdapter = new BoxingViewportAdapter(this.Window, this.graphics, 1280, 720);
 
-			UserInterface.Initialize(this.Content, BuiltinThemes.hd);
-
 			// Initialize the first scene
 			activeScene.Initialize();
-        }
+		}
 
-        protected override void LoadContent()
-        {
-            drawer = new SpriteBatch(GraphicsDevice);
-        }
+		protected override void LoadContent()
+		{
+			drawer = new SpriteBatch(GraphicsDevice);
+		}
 
-        protected override void UnloadContent()
-        {
-        }
+		protected override void UnloadContent()
+		{
+		}
 
-        protected override void Update(GameTime gameTime)
-        {
-            base.Update(gameTime);
+		protected override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
 
 			Time.GameTime = gameTime;
 			Input.Update();
 			Tasks.Update();
-			
+
 			UserInterface.Active.Update(gameTime);
 			activeScene.Update();
-
 
 			Colliders.Update();
 		}
 
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(activeScene.ClearColor);
-			
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(activeScene.ClearColor);
+
 			Time.GameTime = gameTime;
 
 			drawer.Begin(transformMatrix: activeScene.Cam.Camera2D.GetViewMatrix(), samplerState: SamplerState.PointClamp);
@@ -120,11 +118,10 @@ namespace Prime
 			// Draw GeonBit's UI
 			var vp = GraphicsDevice.Viewport;
 			GraphicsDevice.Viewport = new Viewport(GraphicsDevice.PresentationParameters.Bounds);
-			UserInterface.Active.UseRenderTarget = true;
 			UserInterface.Active.Draw(drawer);
 			GraphicsDevice.Viewport = vp;
 
-            base.Draw(gameTime);
-        }
-    }
+			base.Draw(gameTime);
+		}
+	}
 }
